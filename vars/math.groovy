38d4206d0e4908478f986cb9433a9e295a9ae3b4
kpraneet18 <103165894+kpraneet18@users.noreply.github.com>
2022-10-20 21:03:18 +0530

    Create math.groovy

---
+++ vars/math.groovy
@@ -0,0 +1,14 @@
+def sum(a,b)
+{
+echo "the sum of  ${a} and ${b} is ${a+b}"
+}
+
+def subtract(a,b)
+{
+echo "the subtraction of  ${a} and ${b} is ${a-b}"
+}
+
+def multiply(a,b)
+{
+echo "the product of  ${a} and ${b} is ${a*b}"
+}
